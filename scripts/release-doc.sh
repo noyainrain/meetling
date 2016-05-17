2599296f4d6b35c691954e38c2e2aa0c20ee96ab
Sven <sven.jms@gmail.com>
2016-05-17 15:56:36 +0200

    MORE

--- scripts/release-doc.sh
+++ scripts/release-doc.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
+# TODO: publish at something like docs.meetling.org
+
 set -e
 
-# Update gh-pages
-git checkout gh-pages
-git fetch
-git merge
+#REPO=git@github.com:meetling/meetling.github.io.git
+#BRANCH=master
+REPO=$(git config remote.origin.url)
+BRANCH=gh-pages
+DOCPATH="$PWD/doc/build"
+PAGESPATH=/tmp/gh-pages
+
+rm -rf $PAGESPATH
+git clone --branch=$BRANCH --single-branch $REPO $PAGESPATH
+
+cd $PAGESPATH
 
 # Update documentation
 git rm -r .
-cp -r doc/build/* .
+cp -r "$DOCPATH"/* .
 touch .nojekyll
-git add $(ls doc/build) .nojekyll
+git add -A
+
+git diff --cached --quiet && exit
 
 # Publish
 git commit -m "Update documentation"
 git push
-
-git checkout master

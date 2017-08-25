9ac75814b015b7300799ef492cf3df1c6e9875f8
Sven <sven@inrain.org>
2017-08-25 20:02:48 +0200

    WIP

--- micro/tests/test_message.py
+++ micro/tests/test_message.py
@@ -6,6 +6,73 @@ class EmailChannelTest(TestCase):
     def setUp(self):
         self.smtp_url = '//localhost:8025'
 
+        hub = MessageHub(services={
+            'email': {
+                'smtp_url': '//localhost:8025',
+                'sender': 'message@example.org',
+                'render_auth_msg': lambda e, a: 'Subject: X\n\nY\n'
+            },
+            'telegram': {
+                'api_token': '188...xrc'
+            }
+        })
+
+        # AuthRequest = (uid?, code)
+        #               uid only needed for (email, sms, ...) not (tg, oauth, ...)
+        # Channel = (uid, token?)
+        #           token not needed for (email, sms, ...) only (tg, oauth, ...)
+        #           uid is just additional info for (tg, oauth, ...), not needed to communicate
+
+        # email (stronly)
+        auth_request = hub.connect('email:happy@example.org')
+        write(op.id + '.auth_request', auth_request)
+        # ... auth_request_id, auth via UI
+        auth_request = read(op.id + '.auth_request')
+        channel = hub.finish_connect(auth_request, auth)
+        write(user.id + '.channel', channel)
+        # ...
+        channel = read(user.id + '.channel')
+        hub.send(channel, 'Subject: Foo\n\nFoo\n')
+
+        # email (Channel)
+        auth_request = hub.connect('email:happy@example.org')
+        write(op.id + '.auth_request', auth_request)
+        # ... auth via UI
+        auth_request = read(op.id + '.auth_request')
+        channel = hub.finish_connect(auth_request, auth)
+        write(user.id + '.channel', str(channel))
+        # ...
+        channel = Channel(read(user.id + '.channel'), hub) # alt: hub.make_channel(read(...))
+        channel.send('Subject: Foo\n\nFoo\n')
+
+        # email (Channel + AuthRequest)
+        auth_request = hub.connect('email:happy@example.org')
+        write(op.id + '.auth_request', str(auth_request))
+        # ... auth via UI
+        auth_request = AuthRequest(read(op.id + '.auth_request'), hub)
+        channel = auth_request.finish(auth)
+        write(user.id + '.channel', str(channel))
+        # ...
+        channel = Channel(read(user.id + '.channel'), hub)
+        channel.send('Subject: Foo\n\nFoo\n')
+
+        # ---
+
+        # telegram (stronly)
+        auth_request = hub.connect('telegram')
+        channel = hub.finish_connect(auth_request, '')
+        hub.send(channel, 'Foo')
+
+        # telegram (Channel)
+        auth_request = hub.connect('telegram')
+        channel = hub.finish_connect(auth_request, '')
+        channel.send('Foo')
+
+        # telegram (Channel + AuthRequest)
+        auth_request = hub.connect('telegram')
+        channel = auth_request.finish('')
+        channel.send('Foo')
+
     def test_auth(self):
         code = None
         def _render(email, auth):
